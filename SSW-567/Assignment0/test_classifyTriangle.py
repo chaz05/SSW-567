5da22a237ed98ee7adff3db38af8a9fe994ddf37
tjfloyd <tjfloyd@BLM00060.rockwellcollins.com>
2017-01-23 07:47:18 -0800

    Added two tests with questionable inputs

--- SSW-567/Assignment0/test_classifyTriangle.py
+++ SSW-567/Assignment0/test_classifyTriangle.py
@@ -4,27 +4,34 @@ Created on Jan 21, 2017
 @author: caadams2
 '''
 import unittest
-from solveTriangle import classifyTriangle
+import solveTriangle as Tony
+import tjfTriangle as Travis
 from math import sqrt
 
 class Test(unittest.TestCase):
 
 
     def testNominalScalene(self):
-        self.assertEqual(classifyTriangle(3, 4, 5, 0), (True, False, False, True))
-        
-        self.assertEqual(classifyTriangle(1, 2, 3), (True, False, False, False))
+        for member in [Tony, Travis]:     
+            self.assertEqual(member.classifyTriangle(3, 4, 5, 0), (True, False, False, True))
+            
+            self.assertEqual(member.classifyTriangle(1, 2, 3), (True, False, False, False))
         
     def testNominalIsosceles(self):
-        #right
-        self.assertEqual(classifyTriangle(4,4, 4*sqrt(2)), (False, True, False, True))
-        #not right
-        self.assertEqual(classifyTriangle(2, 2, 5), (False, True, False, False))
+        for member in [Tony, Travis]:
+            #right
+            self.assertEqual(member.classifyTriangle(4,4, 4*sqrt(2)), (False, True, False, True))
+            #not right
+            self.assertEqual(member.classifyTriangle(2, 2, 5), (False, True, False, False))
         
     def testNominalEquilateral(self):
-        self.assertEqual(classifyTriangle(2, 2, 2), (False, False, True, False))
-
-
+        for member in [Tony, Travis]:
+            self.assertEqual(member.classifyTriangle(2, 2, 2), (False, False, True, False))
+    
+    def testInvalidInputs(self):
+        for member in [Tony, Travis]:
+            self.assertEqual(member.classifyTriangle(0, 0, 0), (False, False, False, False))
+            self.assertEqual(member.classifyTriangle(-1, 2, 5), (True, False, False, False))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testNominalScalene', 'Test.testNominalIsosceles', 'Test.testNominalEquilateral']
