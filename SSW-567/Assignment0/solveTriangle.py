877ba6130edc59b7dbef5404a5a63e571e19820a
cbthaden <cbthaden@CRP25003.rockwellcollins.com>
2017-01-23 09:58:13 -0600

    added special comment

--- SSW-567/Assignment0/solveTriangle.py
+++ SSW-567/Assignment0/solveTriangle.py
@@ -18,7 +18,7 @@ def classifyTriangle(a, b, c, res = 0.000005):
     isosceles = ((a == b) or (a == c) or (b == c)) and not (a == b and a == c)
     equilateral = (a == b and a == c)
     rightTriangle = (a**2 + b**2)+res >= c**2 and (a**2 + b**2)-res <= c**2
-    
+    # special triangle assignment
     print("CAA: Scalene: %s,\tIsosceles %s, \tEquilateral: %s,\tRight Triangle: %s" % (scalene, isosceles, equilateral, rightTriangle))
     return scalene, isosceles, equilateral, rightTriangle
 
