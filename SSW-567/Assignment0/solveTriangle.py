cda1338bd561d9bb913ecafb26e63071071a6062
caadams2 <caadams2@CRM16159.rockwellcollins.com>
2017-01-23 09:50:37 -0600

    Updating print statment

--- SSW-567/Assignment0/solveTriangle.py
+++ SSW-567/Assignment0/solveTriangle.py
@@ -19,7 +19,7 @@ def classifyTriangle(a, b, c, res = 0.000005):
     equilateral = (a == b and a == c)
     rightTriangle = (a**2 + b**2)+res >= c**2 and (a**2 + b**2)-res <= c**2
     
-    print("Scalene: %s,\tIsosceles %s, \tEquilateral: %s,\tRight Triangle: %s" % (scalene, isosceles, equilateral, rightTriangle))
+    print("CAA: Scalene: %s,\tIsosceles %s, \tEquilateral: %s,\tRight Triangle: %s" % (scalene, isosceles, equilateral, rightTriangle))
     return scalene, isosceles, equilateral, rightTriangle
 
 
